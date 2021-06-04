c652b42584f87715d4e37ebd532eb0dc046146ff
Emil Frisk <emilnygaardfrisk@gmail.com>
2021-06-04 23:19:06 +0200

    updated datamodel

--- database/create_database.sql
+++ database/create_database.sql
@@ -1,5 +1,15 @@
 CREATE TABLE IF NOT EXISTS post (
   id INT AUTO_INCREMENT PRIMARY KEY,
+	
+	-- apartment information
+	size_m2 SMALLINT,
+	nr_rooms SMALLINT,
+	price INT,
+	accomodation_type VARCHAR(10), -- enum
+	availability_type VARCHAR(10), -- enum
+	months_available SMALLINT,
+
+	-- address
   street VARCHAR(200),
   street_number VARCHAR(5),
   floor VARCHAR(3),
@@ -7,9 +17,13 @@ CREATE TABLE IF NOT EXISTS post (
   city VARCHAR(50),
   zipcode VARCHAR(10),
   country VARCHAR(5),
+
+	-- landlord information
   landlord_phone VARCHAR(20),
   landlord_email VARCHAR(100),
   landlord_name VARCHAR(100),
+
+	key VARCHAR(36), -- guid
   created_on TIMESTAMP,
-  updated_on TIMESTAMP
+  updated_on TIMESTAMP,
 )
